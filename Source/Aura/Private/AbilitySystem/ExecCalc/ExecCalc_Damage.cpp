a07787a3ff6d6caa55aaf691d7fca456424ac859
andresbr92 <andresbr92@gmail.com>
2025-04-13 16:14:41 +0200

    Critical hit chance and critical hist resistance calculation

--- Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Aura/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -15,11 +15,20 @@ struct AuraDamageStatics
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitChance);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitDamage);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(CriticalHitResistance);
 	AuraDamageStatics()
 	{
+		// Target Attributes
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, Armor, Target, false);
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, BlockChance, Target, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, CriticalHitResistance, Target, false);
+		// Source Attributes 
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, ArmorPenetration, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, CriticalHitChance, Source, false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet, CriticalHitDamage, Source, false);
+		
 	}
 	
 };
@@ -34,6 +43,9 @@ UExecCalc_Damage::UExecCalc_Damage()
 	RelevantAttributesToCapture.Add(GetAuraDamageStatics().ArmorDef);
 	RelevantAttributesToCapture.Add(GetAuraDamageStatics().BlockChanceDef);
 	RelevantAttributesToCapture.Add(GetAuraDamageStatics().ArmorPenetrationDef);
+	RelevantAttributesToCapture.Add(GetAuraDamageStatics().CriticalHitChanceDef);
+	RelevantAttributesToCapture.Add(GetAuraDamageStatics().CriticalHitDamageDef);
+	RelevantAttributesToCapture.Add(GetAuraDamageStatics().CriticalHitResistanceDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -54,34 +66,65 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	EvaluationParameters.SourceTags = SourceTags;
 	EvaluationParameters.TargetTags = TargetTags;
 
-	// Base Damage
+	/**
+	 * Base Damage
+	 */ 
 	float Damage = EffectSpec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
-	// Calculate Block Chance
-	float TargetBlockChance;
+	/**
+	 * Block Chance
+	 */ 
+	float TargetBlockChance = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().BlockChanceDef, EvaluationParameters, TargetBlockChance);
 	TargetBlockChance = FMath::Max<float>(0.f, TargetBlockChance);
 	bool bBlocked = FMath::RandRange(0, 100) < TargetBlockChance;
 	Damage = bBlocked && Damage /2.f;
-	
+	/**
+	 * Armor Damage reduction
+	 */ 
 	float TargetArmor = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().ArmorDef, EvaluationParameters,TargetArmor);
 	TargetArmor = FMath::Max<float>(0.f, TargetArmor);
-	// Calculate damage after armor
-	
-	
 	
 
-	// Calculate armor penetration
-	float SourceArmorPenetration;
+	/**
+	 * Block Chance
+	 */ 
+	float SourceArmorPenetration = 0.f;
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().ArmorPenetrationDef, EvaluationParameters, SourceArmorPenetration);
 	SourceArmorPenetration = FMath::Max<float>(0.f, SourceArmorPenetration);
 	CONSOLE_LOG("Aura Armor Penetration: %f", SourceArmorPenetration);
 	const float EffectiveArmor = TargetArmor += (100 - SourceArmorPenetration * 0.25f) / 100.f;
 
 	Damage *= (100.f - EffectiveArmor) / 100.f;
+
+	/**
+	 * Critical Hit Chance and Critical Hit bonus
+	 */
+	float SourceCriticalHitChance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().CriticalHitChanceDef, EvaluationParameters, SourceCriticalHitChance);
+	CONSOLE_LOG("Critical hit chance: %f", SourceCriticalHitChance);
+	SourceCriticalHitChance = FMath::Max<float>(0.f, SourceCriticalHitChance);
+
+
+	float SourceCriticalHitDamage = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().CriticalHitDamageDef, EvaluationParameters, SourceCriticalHitDamage);
+	SourceCriticalHitDamage = FMath::Max<float>(0.f, SourceCriticalHitDamage);
+	
+	float TargetCriticalHitResistance = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(GetAuraDamageStatics().CriticalHitDamageDef, EvaluationParameters, TargetCriticalHitResistance);
+	TargetCriticalHitResistance = FMath::Max<float>(0.f, TargetCriticalHitResistance);
+
+	const float EffectiveCriticalHitChance = SourceCriticalHitChance - TargetCriticalHitResistance * 0.15f;
 	
 
-	// Calculate Armor
+	bool bIsCriticalHit = FMath::RandRange(0.f, 100.f) < EffectiveCriticalHitChance;
+
+	if (bIsCriticalHit)
+	{
+		Damage = Damage * 2 + SourceCriticalHitDamage;
+		CONSOLE_LOG("is Crital hit: %f", Damage);
+	}
+	
 	
 	
 	FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(), EGameplayModOp::Additive, Damage);
